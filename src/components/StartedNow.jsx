cd1b599cc7ba414417c80c4909b683cd77e022e9
ShaikAyub7 <skayub929@gmail.com>
2024-09-21 20:00:00 +0530

    .

--- src/components/StartedNow.jsx
+++ src/components/StartedNow.jsx
@@ -10,9 +10,17 @@ const StartedNow = () => {
           Ready to optimize your browsing time and improve productivity?
           Download Tab Time Tracker today and take the first step toward better
           time management online.
-          <div style={{ marginTop: "2rem" }} className="footerBtn">
-            <img src={img2} alt="" style={{ width: "30px" }} />
-            <a href="">Download Now</a>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <div style={{ marginTop: "2rem" }} className="footerBtn">
+              <img src={img2} alt="" style={{ width: "30px" }} />
+              <a href="">Download Now</a>
+            </div>
           </div>
         </div>
       </div>
