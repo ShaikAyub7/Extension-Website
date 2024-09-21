f650c5f5156715e1d200da0f77cf87ecdde9b5a9
ShaikAyub7 <skayub929@gmail.com>
2024-09-21 21:09:27 +0530

    .

--- src/Data.jsx
+++ src/Data.jsx
@@ -4,5 +4,5 @@ export const NavbarData = [
   { id: 1, text: "Home", href: "#hero" },
   { id: 2, text: "About", href: "#about" },
   { id: 3, text: "Contact", href: "#contact" },
-  { id: 4, text: "Download", href: "#hero" },
+  { id: 4, text: "Download", href: "#download" },
 ];
